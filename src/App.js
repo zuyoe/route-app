cd9d85ea5ec81fd77ea715aa9f9852898eb43ee2
zuyoe <9xtwo5@naver.com>
2022-12-15 18:02:33 +0900

    axios 추가 밑 useMatch 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+// axios API
+import instance from "./api/axios";
+import requests from "./api/request";
+
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -16,67 +20,29 @@ const App = () => {
   //  배열은 [state, state업데이트 함수] 돌려받는다.
   //  배열은 [getter, setter] 돌려받는다.
   //  useState(초기값) : 초기값이라 함은 state 의 초기값
-  const [members, setMembers] = useState([
-    { name: "Maggie Adams", photo: "photos/Mag.png" },
-    { name: "Sammie Purcell", photo: "photos/Sam.png" },
-    { name: "Tim Purcell", photo: "photos/Tim.png" },
-    { name: "Scott King", photo: "photos/King.png" },
-    { name: "Johnny Pike", photo: "photos/JPike.jpg" },
-    { name: "Toby Ruckert", photo: "photos/Toby.jpg" },
-  ]);
+  const [members, setMembers] = useState([]);
 
   // 플레이 리스트 State
   // 화면을 Re-Render 할 수 있는 조건은 state / props 변경
-  const [songs, setSongs] = useState([
-    {
-      id: 1,
-      title: "Fallin' for you",
-      musician: "Colbie callet",
-      youtube_link: "PABUl_EX_hw",
-    },
-    {
-      id: 2,
-      title: "Can't hurry love",
-      musician: "The supremes",
-      youtube_link: "EJDPhjQft04",
-    },
-    {
-      id: 3,
-      title: "Landslide",
-      musician: "Dixie chicks",
-      youtube_link: "V2N7gYom9-A",
-    },
-    {
-      id: 4,
-      title: "Can't let go",
-      musician: "Linda ronstadt",
-      youtube_link: "P-EpGKXmoe4",
-    },
-    {
-      id: 5,
-      title: "Doctor my eyes",
-      musician: "Jackson Browne",
-      youtube_link: "7JlFKS_1oZk",
-    },
-    {
-      id: 6,
-      title: "We gotta get you a woman",
-      musician: "Todd Rundgren",
-      youtube_link: "EyUjbBViAGE",
-    },
-    {
-      id: 7,
-      title: "Hip to my heart",
-      musician: "Band Perry",
-      youtube_link: "vpLCFnD9LFo",
-    },
-    {
-      id: 8,
-      title: "Rolling in the deep",
-      musician: "Adele",
-      youtube_link: "EvK8pDK6IQU",
-    },
-  ]);
+  const [songs, setSongs] = useState([]);
+
+  //  외부데이터 가져오기
+  const fetchData = async () => {
+    // 멤버목록 가져오기
+    const resultMember = await instance.get(requests.fetchMember);
+    setMembers(resultMember.data);
+    // song목록 가져오기
+    const resultSong = await instance.get(requests.fetchSong);
+    setSongs(resultSong.data);
+
+
+    // setMembers();
+    // setSongs();
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <Router>
